129304b7fac0f68eae13f9888947f41a50d17a78
Matthieu-Simon <simon.matthieu89@gmail.com>
2023-02-10 16:09:10 +0100

    fix: ajout des bullets en js

--- assets/script.js
+++ assets/script.js
@@ -15,15 +15,38 @@ const slides = [
 		"image":"slide4.png",
 		"tagLine":"Autocollants <span>avec découpe laser sur mesure</span>"
 	}
-]
-console.log(slides.length);
+];
 
-/* Ajout Event "click" */
-const left = document.getElementById("btn-left").addEventListener("click", function() {
+// Les variables qui ciblent les différents éléments du DOM
+const banner = document.getElementById('#banner');
+const arrowLeft = document.querySelector('.arrow_left');
+const arrowRight = document.querySelector('.arrow_right');
+
+// On compte le nombre d'éléments du tableau slides
+const numberOfSlide = slides.length;
+
+let count = 0;
+
+// Event au clic
+arrowLeft.addEventListener('click', () => {
 	console.log("Gauche");
 });
-
-const right = document.getElementById("btn-right").addEventListener("click", function() {
+arrowRight.addEventListener('click', () => {
 	console.log("Droite");
 });
 
+// Fonction pour créer les bullets
+function createBullet() {
+	const dots = document.querySelector('.dots');
+	// on boucle sur le tableau slides pour créer le nombre exact de bullets
+	for (let i = 0; i < numberOfSlide; i++) {
+		// on crée un élément span
+		const dot = document.createElement('span');
+		// on applique le style css "dot"
+		dot.classList.add('dot');
+		// on ajoute les éléments "dot" dans le parent dots
+		dots.appendChild(dot);
+	}
+}
+// on appelle la fonction
+createBullet();
